f061a37e0f9924b5a468db768a4f800e08d01cb8
Arunakumara-s <arunakumara.sp@gmail.com>
2023-06-24 23:15:48 +0530

    Merging Two Sorted Array

---
+++ Datastructure and Algorithms/src/com/practice/MergeTwoArray.java
@@ -0,0 +1,27 @@
+package com.practice;
+
+import java.util.Arrays;
+
+public class MergeTwoArray {
+	public static void main(String[] args) {
+		int firstArray[] =  { 1, 3, 4, 5}, secondArray[] = {2, 4, 6, 8} ;
+		int size = firstArray.length + secondArray.length;
+		int result[] = new int[size];
+		Arrays.sort(firstArray);
+		Arrays.sort(secondArray);
+		int count = 0;
+		
+		for(int i = 0; i < firstArray.length; i++) {
+			result[count++] = firstArray[i];
+		}
+		
+		for(int j = 0; j < secondArray.length; j++) {
+			result[count++] = secondArray[j];
+		}
+		
+		for(int i = 0; i < size; i++) {
+			System.out.print(result[i]+" ");
+		}
+	}
+
+}

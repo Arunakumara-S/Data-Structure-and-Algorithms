c3b7a21936500a77296fb507176ad22b05a917bf
Arunakumara-s <arunakumara.sp@gmail.com>
2023-06-27 23:00:38 +0530

    Permutation of a given string

---
+++ Datastructure and Algorithms/src/com/practice/PermutationOfAString.java
@@ -0,0 +1,25 @@
+package com.practice;
+
+public class PermutationOfAString {
+	public static void main(String args[]) {
+		String str = "ABC";
+		permute(str, " ");
+		
+	}
+	public static void permute(String str, String ans) {
+		if(str.length() == 0) {
+			System.out.println(ans + " ");
+			return;
+		}
+		else
+		{
+			for(int i = 0; i < str.length(); i++) {
+				char ch = str.charAt(i);
+				String ros = str.substring(0, i) + str.substring(i + 1);
+				permute(ros, ans + ch);
+			}
+		}
+		
+	}
+
+}

5427060c19afc913c7301930e62a10e65ea078ac
Arunakumara-s <arunakumara.sp@gmail.com>
2023-07-08 13:28:06 +0530

    Solution For Roman Integer Code

---
+++ Datastructure and Algorithms/src/com/practice/RomanInteger.java
@@ -0,0 +1,29 @@
+package com.practice;
+
+public class RomanInteger {
+	public static void main(String args[]) {
+		System.out.println(romanToInteger("MCMXCIV"));
+		
+	}
+	public static int romanToInteger(String s) {
+		int ans = 0, num = 0;
+		for(int i = s.length() - 1; i >= 0; i--) {
+			switch(s.charAt(i)) {
+			case 'I' : num = 1; break;
+			case 'V' : num = 5; break;
+			case 'X' : num = 10; break;
+			case 'L' : num = 50; break;
+			case 'C' : num = 100; break;
+			case 'D' : num = 500; break;
+			case 'M' : num = 1000; break;
+			
+			}
+			if(num < ans)
+				ans = ans - num;
+			else
+				ans = ans + num;
+		}
+		return ans;
+	}
+
+}
